105b0b95f85e4c9c724e0dae3de065e3a9ebcbbd
filip k <filip2006kujawa@gmail.com>
2023-10-13 15:17:27 +0200

    funkcje do js

---
+++ script.js
@@ -0,0 +1,5 @@
+const fetchSongs = async () => {
+    const response = await fetch (https://gist.githubusercontent.com/techniadrian/c39f844edbacee0439bfeb107227325b/raw/81eec7847b1b3dfa1c7031586405c93e9a9c1a2d/songs.json)
+}
+
+const fetchGenres = async () => {}
